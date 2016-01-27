da0c93b461ce1de308fc1036cf554fb28c987927
Todd Benson <todd.benson@csaa.com>
2016-01-27 14:49:17 -0700

    New Files

---
+++ Setup.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/nmap/nmap.git /Applications/nmap
+git clone https://github.com/sqlmapproject/sqlmap.git /Applications/sqlmap
+git clone https://github.com/spinkham/skipfish.git /Applications/skipfish
+git clone https://github.com/sullo/nikto.git /Applications/nikto
+git clone https://github.com/danielmiessler/SecLists.git ~/Documents/SecList
+git clone https://github.com/drwetter/testssl.sh.git /Applications/testSSL
+git clone https://github.com/ToddBenson/SecurityDocs.git ~/Documents/Security
+git clone https://github.com/ToddBenson/configFiles.git ~/Documents/config
+## Download Wireshark, SoapUI, ZAP, and Burp
+## Firefox Plugins - Web Developer, Tamper Data, Foxy Proxy, Cookie Manager, XSS Me
